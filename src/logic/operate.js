f7117f2fe86c118691304f2145593e4bf56678ed
Daniel Larbi Addo <addodaniellarbi@gmail.com>
2020-06-22 14:34:49 +0000

    replace require with import

--- src/logic/operate.js
+++ src/logic/operate.js
@@ -1,4 +1,4 @@
-const Big = require('big.js');
+import Big from 'big.js';
 
 const Operate = (num1, num2, operation) => {
   const num1Val = Big(num1);
@@ -13,7 +13,7 @@ const Operate = (num1, num2, operation) => {
       result = num1Val.minus(num2Val).toString();
       break;
     case '÷':
-      if (num1Val.toString() === '0' || num2Val.toString() === '0') {
+      if (num2Val.toString() === '0') {
         result = "Can't divide by 0";
       } else {
         result = num1Val.div(num2Val).toString();
