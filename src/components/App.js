6a1db60fb52c16a05e1ef6db2b4b901903cc80ea
Daniel Larbi Addo <addodaniellarbi@gmail.com>
2020-06-24 07:41:56 +0000

    initialize state with three properties in constructor

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,29 @@
 import React from 'react';
-// import Calculate from '../logic/calculate';
+import Calculate from '../logic/calculate';
 
 import Display from './Display';
 import ButtonPanel from './ButtonPanel';
 
-const App = () => (
-  <div className="App common-width">
-    <Display />
-    <ButtonPanel />
-  </div>
-);
 
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      result: null,
+      nextVal: null,
+      operation: null
+    };
+
+    this.buttonClick = this.buttonClick.bind(this);
+  }
+
+  render() {
+    return(
+      <div className="App common-width">
+        <Display />
+        <ButtonPanel />
+      </div>
+    )
+  }
+}
 export default App;
